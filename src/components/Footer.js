9532b618c437ded79ab1b97df773d5b25c418a1f
Omar Khalil <neuronmaze@gmail.com>
2023-05-01 20:50:54 -0400

    first edition

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="leftsidelinks">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
